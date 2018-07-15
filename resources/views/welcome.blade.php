4fdc549abcb0e20ab921ee57218efbb580fff30e
wesley <wesleyhjpr@gmail.com>
2018-07-15 01:31:24 -0300

    add HTML & FORMS and make:auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
                     Laravel
                 </div>
 
-                <div class="links">                                        
+                <div class="links">  
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
