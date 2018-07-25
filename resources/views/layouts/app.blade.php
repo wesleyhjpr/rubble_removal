e6ede96d758e37e263e6aa447504e9e4907d2263
wesley <wesleyhjpr@gmail.com.br>
2018-07-25 19:47:13 -0300

    add redirect to route /clientes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -356,7 +356,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    <a class="dropdown-item" style="color: #5f5f5f !important;"
+                                    href="{{ route('logout') }}"
                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
